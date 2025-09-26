e8d44521be82f554da9b8c18508020c2473d7516
hiengianguyen <hiennguyen2060@gmail.com>
2025-09-26 20:50:58 +0700

    Not found data ui

--- src/Pages/Admin/Students/Component/BarDivideClass/index.jsx
+++ src/Pages/Admin/Students/Component/BarDivideClass/index.jsx
@@ -8,10 +8,13 @@ import Col from "react-bootstrap/esm/Col";
 import DroppableClass from "../../../../../Components/DroppableClass";
 import { CSSTransition } from "react-transition-group";
 import Loading from "../../../../../Components/Loading";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 const cx = classNames.bind(style);
 
-function BarDivideClass({ show = false}) {
+function BarDivideClass({ show = false }) {
   const [classes, setClasses] = useState([]);
   const [isLoadingList, setIsLoadingList] = useState(true);
   const nodeRef = useRef();
@@ -38,14 +41,27 @@ function BarDivideClass({ show = false}) {
         </div>
         <div className={cx("body", "position-relative")}>
           <Row className="gap-4">
-            {classes &&
+            {classes.length ? (
               classes.map((classItem, index) => (
                 <Col xs={"auto"} key={index} style={{ width: "max-content" }}>
                   <DroppableClass id={classItem.id}>
                     <CardClass data={classItem} isDropped={show} />
                   </DroppableClass>
                 </Col>
-              ))}
+              ))
+            ) : (
+              <div className="d-flex flex-column align-items-center">
+                <img src="/empty-folder.png" alt="" style={{ height: "10pc", width: "10pc", pointerEvents: "none", userSelect: "none" }} />
+                <div className="d-flex text-center flex-column">
+                  <h3>Không có lớp học</h3>
+                  <i className="text-secondary">Hiệi tại chưa có lớp học trong hàng đợi</i>
+                  <Link to="/ad/classmate" className="text-primary">
+                    Quản lí lớp học
+                    <FontAwesomeIcon icon={faArrowRight} className="ms-2" />
+                  </Link>
+                </div>
+              </div>
+            )}
           </Row>
           {isLoadingList && <Loading height="100%" position="absolute" color="rgb(244 244 244)" zIndex="9998" />}
         </div>
